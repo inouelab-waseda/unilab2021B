bb3d266b4bb3c0c05587ea4fc818d68e79245355
hiroto-murakami <55971234+hiroto-murakami@users.noreply.github.com>
2021-04-06 14:47:33 +0900

    first commit

---
+++ Assets/Scripts/press_to_setting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class press_to_setting : MonoBehaviour
+{
+    private bool firstPush = false;
+
+    public void PressStart()
+    {
+        Debug.Log("Press Start!");
+        if (!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            SceneManager.LoadScene(2);
+            firstPush = true;
+        }
+    }
+}
