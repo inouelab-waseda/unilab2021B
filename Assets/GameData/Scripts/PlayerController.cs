506c3882d2dc29b44efd41c3b3c6f6831d191139
kaialex <kalexinjp@gmail.com>
2021-09-16 15:59:40 +0900

    PlayerControllerの追加

---
+++ Assets/GameData/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject player;
+
+    public Vector2 GetPlayerpos() => player.transform.position;
+
+    public void SetPlayerpos(Vector2 pos)
+    {
+        if (player == null) FindPlayer();
+        Debug.Log(player);
+        player.transform.position = pos;
+    }
+
+    private void FindPlayer()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void PlayerMove()
+    {
+        //UniRxでデータを購読し、それに合わせてプレイヤーの動きを決める
+
+    }
+}
